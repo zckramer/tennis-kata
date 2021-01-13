9afe25b84d40d1900806cbc9c97abf44619af254
Zachary Kramer <zckramer83@gmail.com>
2021-01-12 19:57:31 -0500

    set up first test

---
+++ Tennis.js
@@ -0,0 +1,32 @@
+// In a game of tennis, a player begins with a score of zero (0). 
+// With each success, the player earns more points. The points are 
+// earned in this sequence:
+
+const { builtinModules } = require("module");
+
+// 0
+// 15
+// 30
+// 40
+
+// If a player has 40 and scores again, that player wins the game 
+// as long as the other player does not also have 40 points. If both 
+// players reach 40 points it is referred to as a 'deuce.'
+
+// Scoring during deuce give a player advantage. If the other player 
+// scores again, the score returns to deuce. If a player has advantage 
+// and scores again, that player wins the game.
+
+
+// Players must be able to score points
+// The game must be able to be completed with a winner
+// The 'deuce' case should be handled
+// After a game has been won, a winner must be determined
+// The current score of either player should be available at any point during the game
+const Game = () => {
+    let player1Score = 0;
+    let player2Score = 0;
+    function 
+
+}
+module.exports = Game;
---
+++ Tennis.test.js
@@ -0,0 +1,11 @@
+const Game = require('./Tennis');
+
+test('each player has score of zero ', () => {
+    const underTest = Game();
+    expect(underTest).toEqual(0);
+    
+})
+// test('should ', () => {
+    
+// })
+
